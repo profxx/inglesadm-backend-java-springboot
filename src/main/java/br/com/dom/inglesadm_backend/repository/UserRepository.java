59aaccf89f079e8a2f122c859a17ebca08b481b7
Alexandre Pimentel <acpimentel@gmail.com>
2025-04-23 20:56:08 -0300

    Ajuste CORS

--- src/main/java/br/com/dom/inglesadm_backend/repository/UserRepository.java
+++ src/main/java/br/com/dom/inglesadm_backend/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import br.com.dom.inglesadm_backend.model.User;
 
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByLogin(String login);
 }
